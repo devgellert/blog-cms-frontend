6fc8effe857211f147cf2f984cf5837effecaf0c
Puskás Gellért <devgellert@gmail.com>
2022-03-09 17:47:17 +0100

    [lib] mui lib installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { FC, memo } from "react";
+import {Button} from "@mui/material";
 
 type Props = {};
 
 const App: FC<Props> = ({}) => {
  return <div>
   <h1>{process.env.REACT_APP_API}</h1>
+
+  <Button variant="text">Text</Button>
+  <Button variant="outlined">Outlined</Button>
+
+
  </div>;
 }
 
