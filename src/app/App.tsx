37e80bf33e74ca60f780ad51c7d02cf6e6087669
Puskás Gellért <devgellert@gmail.com>
2022-03-10 19:58:04 +0100

    [sass] setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { FC, memo } from "react";
 import { Provider } from "react-redux";
 //
-import store from "./redux/store";
+import store from "../redux/store";
+import css from "./App.module.scss";
 
 type Props = {};
 
 const App: FC<Props> = ({}) => {
     return (
         <Provider store={store}>
-            <div>Hello world</div>
+            <div className={css["App"]}>Hello world</div>
         </Provider>
     );
 };
