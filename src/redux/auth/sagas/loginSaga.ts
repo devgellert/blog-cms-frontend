a08e9768096f3f33b310b9f300040dfeb8b25b12
devGellert <devgellert@gmail.com>
2022-03-10 23:00:22 +0100

    [auth] base logic added

--- src/redux/auth/sagas/loginSaga.ts
+++ src/redux/auth/sagas/loginSaga.ts
@@ -1,7 +1,17 @@
 import { PayloadAction } from "@reduxjs/toolkit";
+import { call } from "redux-saga/effects";
+import api from "../../../api";
 
 function* loginSaga({ payload: { username, password } }: PayloadAction<{ username: string; password: string }>) {
     console.log(username, password);
+
+    // @ts-ignore
+    const response: any = yield call(api.post, '/login', {
+        username,
+        password
+    });
+
+    console.log(response);
 }
 
 export default loginSaga;
